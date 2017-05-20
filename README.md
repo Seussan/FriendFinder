06f67e9c19700381bc111207fa8f4e26c02ceaf7
Seussan <SusanGitHub@gmail.com>
2017-05-20 07:36:05 -0400

    Updated files.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FriendFinder
+
+## A compatibility-based "FriendFinder" application that takes in user survey results, compares the answers with other users and displays name and picture of best overall match. Uses Node and Express Servers.
---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = 
+[{
+	"name": "Sarah",
+	"photo": "http://1yt69vhfz4m3cy6ek2gzn2c1.wpengine.netdna-cdn.com/wp-content/uploads/2014/12/home1.jpg",
+	"scores": [ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
+}, {
+	"name": "Chelsea",
+	"photo": "http://reneemullingslewis.com/wp-content/uploads/2014/08/woman-smiling.png",
+	"scores": [ 4, 2, 5, 1, 3, 2, 2, 1, 3, 2 ]
+}, {
+	"name": "Catherine",
+	"photo": "https://s-media-cache-ak0.pinimg.com/originals/b5/e6/d6/b5e6d6c39a235e9f7b2ec8ef12ee565d.jpg",
+	"scores": [ 5, 2, 2, 2, 4, 1, 3, 2, 5, 5 ]
+}, {
+	"name": "Jacob",
+	"photo": "https://static.pexels.com/photos/26939/pexels-photo-26939.jpg",
+	"scores": [ 3, 3, 4, 2, 2, 1, 3, 2, 2, 3 ]
+}, {
+	"name": "Brutus",
+	"photo": "https://image.freepik.com/free-photo/man-with-long-hair-and-long-beard_1296-270.jpg",
+	"scores": [ 4, 3, 4, 1, 5, 2, 5, 3, 1, 4 ]
+},
+{
+	"name": "Jim",
+	"photo": "http://www.tvovermind.com/wp-content/uploads/2017/04/Yes-Man.jpg",
+	"scores": [ 4, 4, 2, 3, 2, 2, 3, 2, 4, 5 ]
+}];
+
+module.exports = friends;
